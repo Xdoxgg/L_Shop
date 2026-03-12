96a1a1440c036d310d60e79c02457ef4b1d1ddb7
Prandown <140757974+Prandan127@users.noreply.github.com>
2026-03-12 22:22:46 +0300

    basket functional upgrade

--- server/src/controllers/basketController.ts
+++ server/src/controllers/basketController.ts
@@ -16,8 +16,10 @@ class BasketController {
     }
 
     public getUserBasket(req: Request, res: Response): void {
-        const userId = req.query.userId as number;
-        
+        const userId = Number(req.query.userId);
+        if (isNaN(userId)) {
+            res.status(400).json({ error: 'как ' + userId + 'будет вилкой чистить'}) ;
+        return;}
         res.json({'basket': BasketService.getInstance().getUserBasket(userId)})
     }
     
@@ -26,6 +28,33 @@ class BasketController {
         const itemId = req.body.itemId;
         const count = req.body.count;
         
+        if (isNaN(userId) || isNaN(itemId) || isNaN(count) || count <= 0) {
+            res.status(400).json({error: 'wrong data'});
+        return
+    } res.json ({'result': BasketService.getInstance().addItemToBasket(userId, itemId, count)}); 
     }
     
+    public removeItemFromBasket(req: Request, res: Response): void {
+        const userId = Number(req.body.userId);
+        const itemId = Number(req.body.itemId);
+
+        if (isNaN(userId) || isNaN(itemId)) {
+            res.status(400).json({ error: 'wrong id' });
+            return;
+        }
+        const result = BasketService.getInstance().removeItemFromBasket(userId, itemId);
+        res.json({ result });
+    } 
+
+    public clearBasket(req: Request, res: Response): void {
+        const userId = Number(req.body.userId);
+        if (isNaN(userId)) {
+            res.status(400).json({ error: 'wrong id' });
+            return;
+        }
+
+        const result = BasketService.getInstance().clearBasket(userId);
+        res.json({ result });
+    }
+
 }

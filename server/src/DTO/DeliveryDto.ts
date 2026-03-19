5655d2f2f69eeb8e1ab407c5d80ec1a8cde8ef1d
Prandown <140757974+Prandan127@users.noreply.github.com>
2026-03-20 01:43:51 +0300

    deleted decors cuz govno

--- server/src/DTO/DeliveryDto.ts
+++ server/src/DTO/DeliveryDto.ts
@@ -1,7 +1,7 @@
 import { IsNumber, IsString, IsDate, IsEnum, IsOptional, ValidateNested, IsArray } from 'class-validator';
 import { Type } from 'class-transformer';
 import { BasketProduct } from './BasketDto';
-import { AddressDto } from './AddressDto';
+import { AddressDto } from './AdressDto';
 
 export enum DeliveryStatus {
     PENDING = 'pending',
@@ -12,40 +12,14 @@ export enum DeliveryStatus {
 }
 
 export class DeliveryDto {
-    @IsNumber()
     id: number;
-
-    @IsNumber()
     userId: number;
-
-    @IsArray()
-    @ValidateNested({ each: true })
-    @Type(() => BasketProduct)
     basket: BasketProduct[];
-
-    @ValidateNested()
-    @Type(() => AddressDto)
     deliveryAddress: AddressDto;
-
-    @IsDate()
-    @Type(() => Date)
     deliveryDate: Date;
-
-    @IsNumber()
     deliveryPrice: number;
-
-    @IsEnum(DeliveryStatus)
     status: DeliveryStatus;
-
-    @IsOptional()
-    @IsString()
     trackingNumber?: string;
-
-    @IsDate()
-    @Type(() => Date)
     createdAt: Date;
-
-    @IsDate()
-    @Type(() => Date)
     updatedAt: Date;
 }
